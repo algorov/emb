7ef5f944746b0300037938d35613c097735c585c
SEMUL <semul.main@icloud.com>
2023-07-26 22:15:42 +0300

    firmware framework

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -2,75 +2,104 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-mod tm1632;
-mod keyboard;
 
 use defmt::println;
 use embassy_executor::Spawner;
-use embassy_stm32::{self, Peripheral};
-use embassy_stm32::gpio::{AnyPin, Pin};
-use embassy_time::{Duration, Timer};
+use embassy_stm32::{self};
+use embassy_stm32::gpio::{AnyPin, Input, Level, Output, Pull, Speed};
+use embassy_stm32::Peripherals;
 
 use {defmt_rtt as _, panic_probe as _};
+use config::*;
 
-use crate::keyboard::Keyboard;
-use crate::tm1632::LedAndKey;
-use crate::tm1632::config;
-use crate::tm1632::config::BrightnessLevel::{L1, L7};
+use keyboard::*;
+use tm1638::*;
+
+mod config;
+
+struct DivisionByTwo<'d,
+    const DISPLAY_COUNT: usize,
+    const ROW_COUNT: usize,
+    const COLUMN_COUNT: usize,
+    const FONT_CAPACITY: usize> {
+    display: LedAndKey<'d, DISPLAY_COUNT>,
+    keyboard: Keyboard<'d, ROW_COUNT, COLUMN_COUNT, FONT_CAPACITY>,
+}
+
+impl<'d,
+    const DISPLAY_COUNT: usize,
+    const ROW_COUNT: usize,
+    const COLUMN_COUNT: usize,
+    const FONT_CAPACITY: usize> DivisionByTwo<'d, DISPLAY_COUNT, ROW_COUNT, COLUMN_COUNT, FONT_CAPACITY> {
+    fn init(
+        stbs: [Output<'d, AnyPin>; DISPLAY_COUNT],
+        clk: AnyPin,
+        dio: AnyPin,
+        rows: [Output<'d, AnyPin>; ROW_COUNT],
+        columns: [Input<'d, AnyPin>; COLUMN_COUNT],
+        fonts: [u8; FONT_CAPACITY],
+    ) -> DivisionByTwo<'d, DISPLAY_COUNT, ROW_COUNT, COLUMN_COUNT, FONT_CAPACITY> {
+        let display: LedAndKey<'d, DISPLAY_COUNT> = LedAndKey::default(stbs, clk, dio);
+        let keyboard: Keyboard<'d, ROW_COUNT, COLUMN_COUNT, FONT_CAPACITY> = Keyboard::default(rows, columns, fonts);
+
+        Self { display, keyboard }
+    }
+
+    pub fn run(&mut self) -> () {
+        println!("AHAHAHAHAHAHAHAHA");
+    }
+}
 
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Default::default());
-    let mut driver = LedAndKey::new(
-        AnyPin::from(p.PA1),
-        // AnyPin::from(p.PA15),
-        AnyPin::from(p.PA2),
-        AnyPin::from(p.PA3),
-    );
+    let stm: Peripherals = embassy_stm32::init(Default::default());
 
-    let mut array: [u8; config::KEY_COUNT] = [
-        0, 0, 0, 0, 0, 0, 0, 0,
-        // 0, 0, 0, 0, 0, 0, 0, 0,
+    let stbs: [Output<AnyPin>; DISPLAY_COUNT] = [
+        Output::new(AnyPin::from(stm.PA1), Level::High, Speed::Low),
+        Output::new(AnyPin::from(stm.PA15), Level::High, Speed::Low),
+    ];
+    let rows: [Output<AnyPin>; ROW_COUNT] = [
+        Output::new(AnyPin::from(stm.PA8), Level::High, Speed::Low),
+        Output::new(AnyPin::from(stm.PA9), Level::High, Speed::Low),
+        Output::new(AnyPin::from(stm.PA10), Level::High, Speed::Low),
+        Output::new(AnyPin::from(stm.PA11), Level::High, Speed::Low),
+        Output::new(AnyPin::from(stm.PA12), Level::High, Speed::Low),
+    ];
+    let columns: [Input<AnyPin>; COLUMN_COUNT] = [
+        Input::new(AnyPin::from(stm.PA4), Pull::Up),
+        Input::new(AnyPin::from(stm.PA5), Pull::Up),
+        Input::new(AnyPin::from(stm.PA6), Pull::Up),
+        Input::new(AnyPin::from(stm.PA7), Pull::Up),
     ];
 
+    let mut game: DivisionByTwo<DISPLAY_COUNT, ROW_COUNT, COLUMN_COUNT, FONT_CAPACITY> =
+        DivisionByTwo::init(
+            stbs,
+            AnyPin::from(stm.PA2),
+            AnyPin::from(stm.PA3),
+            rows,
+            columns,
+            FONTS,
+        );
 
-    driver.cleanup();
-    driver.set_text_in_segments(0, ['R', ' ', 'U', ' ', 'S', ' ', 'T', ' ']);
-
-    let mut k = Keyboard::default(
-        AnyPin::from(p.PA4),
-        AnyPin::from(p.PA5),
-        AnyPin::from(p.PA6),
-        AnyPin::from(p.PA7),
-        AnyPin::from(p.PA8),
-        AnyPin::from(p.PA9),
-        AnyPin::from(p.PA10),
-        AnyPin::from(p.PA11),
-        AnyPin::from(p.PA12),
-    );
+    game.run();
 
     loop {
-        match k.get_key() {
-            Some(x) => println!("Result: {}", char::from(x)),
-            None => {}
-        }
-        driver.set_led_state(0, 0, 23);
-        driver.def_pressed_keys(&mut array);
-        println!("{:?}", array);
+        // match matrix_keyboard.get_key() {
+        //     Some(x) => println!("Result: {}", char::from(x)),
+        //     None => {}
+        // }
 
-        if array[0] == 1 && array[7] == 1 {
-            driver.set_text_in_segments(0, ['A', 'H', 'A', 'H', 'A', 'H', 'A', 'H']);
-        }
-
-        if array[1] == 1 && array[2] == 1 {
-            driver.set_text_in_segments(0, ['Q', 'U', 'E', 'S', 'O', 'O', 'N', 'N']);
-        }
-        Timer::after(Duration::from_millis(1000)).await;
-        driver.set_brightness(0, L1);
-        Timer::after(Duration::from_millis(1000)).await;
-        driver.set_brightness(0, L7);
-        Timer::after(Duration::from_millis(1000)).await;
-
+        // for id in 0..DISPLAY_COUNT {
+        //     let mut temp: [u8; 8] = [0; 8];
+        //     tm1638.get_pressed_keys(id, &mut temp);
+        //
+        //     for i in 0..8 {
+        //         key_states[i + id * 8] = temp[i];
+        //     }
+        // }
+        //
+        // println!("{:?}", key_states);
     }
 }
